9585a65fcb3b7c25fc0d4104130a6df903ed81da
Abijithkutty <87305206+Abijithkutty@users.noreply.github.com>
2021-10-01 18:29:30 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -25,19 +25,19 @@ Bot Owner Only
 /logger  Get Logs
 /delete Delete File From Database
 
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
+[➡️How To Add Old Files For database](Better Luck Next Time!!)
 
-@Mo_Tech_YT
+@REX_BOTZ
 """
 
 ABOUT = """
 ➥ Name :  Auto Filter With IMDB
     
-➥ Creator : [Muhammed RK](t.me/Mrk_YT)
+➥ Creator : [REX_BOTZ](t.me/REX_BOTZ)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Source Code : [Click Me](https://www.youtube.com/watch?v=5rB7DfNoZ0Y)
 """
